40bfce5e610ff89744652280fe5581ab08ca867d
BrayanC22 <56985044+BrayanC22@users.noreply.github.com>
2021-12-19 14:48:01 -0500

    Version 1

--- LosRapidosSA/CapaDatos/ClsManejador.cs
+++ LosRapidosSA/CapaDatos/ClsManejador.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CapaDatos
 {
-    class Class2
+    public class ClsManejador
     {
     }
 }
---
+++ LosRapidosSA/CapaDatos/ClsParametrosAmbulancia.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class ClsParametrosAmbulancia
+    {
+        private String modelo;
+        private String tipoAmbulancia;  //Asistenciales, no asistenciales.
+        private String placa;
+        private String matricula;
+        private String fechaActivacion;
+        private String estado;
+        private String observacion;
+
+        private List<Object> lst_obj;
+
+        public ClsParametrosAmbulancia() { }
+        public ClsParametrosAmbulancia(String modelo, String tipoAmbulancia, String placa, String matricula, String fechaActivacion, String estado, String observacion)
+        {
+            this.modelo = modelo;
+            this.tipoAmbulancia = tipoAmbulancia;
+            this.placa = placa;
+            this.matricula = matricula;
+            this.fechaActivacion = fechaActivacion;
+            this.estado = estado;
+            this.observacion = observacion;
+
+        }
+
+        //Getter and Setter fields
+        public String Modelo
+        {
+            get { return modelo; }
+            set { modelo = value; }
+        }
+
+        public String TipoAmbulancia
+        {
+            get { return tipoAmbulancia; }
+            set { tipoAmbulancia = value; }
+        }
+
+        public String Placa
+        {
+            get { return placa; }
+            set { placa = value; }
+        }
+
+        public String Matricula
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        public String FechaActivacion
+        {
+            get { return fechaActivacion; }
+            set { fechaActivacion = value; }
+        }
+
+        public String Estado
+        {
+
+            get { return estado; }
+            set { estado = value; }
+        }
+
+        public String Observacion
+        {
+            get { return observacion; }
+            set { observacion = value; }
+        }
+
+    }
+}
